fbc6f04d0b70de254c31810a30bf1a00269971c6
Thomas Delissen <thomasdelissen@MacBook-Pro-de-Thomas.local>
2020-12-27 12:55:06 +0900

    online test : resize

--- js/app.js
+++ js/app.js
@@ -134,5 +134,7 @@ $('#myModal').on('shown.bs.modal', function () {
 window.addEventListener('resize', () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.querySelector('.game-container').style.setProperty('--vh', `${vh}px`);
 });
+let vh = window.innerHeight * 0.01;
+document.querySelector('.game-container').style.setProperty('--vh', `${vh}px`);
